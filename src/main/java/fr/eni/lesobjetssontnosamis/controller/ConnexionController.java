6b78dc0992b2ea3fb4b430e19156f67d7cbac51a
jevariste2024 <jea.evariste@gmail.com>
2025-01-29 12:03:49 +0100

    tout ce que j'ai fait les deux premiers jours

---
+++ src/main/java/fr/eni/lesobjetssontnosamis/controller/ConnexionController.java
@@ -0,0 +1,16 @@
+package fr.eni.lesobjetssontnosamis.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class ConnexionController {
+
+
+@GetMapping("/login")
+public String login(){
+    return "login";
+    }
+}
--- src/main/java/fr/eni/lesobjetssontnosamis/controller/UtilisateurController.java
+++ src/main/java/fr/eni/lesobjetssontnosamis/controller/UtilisateurController.java
@@ -3,9 +3,11 @@ package fr.eni.lesobjetssontnosamis.controller;
 
 import fr.eni.lesobjetssontnosamis.bll.UtilisateurService;
 import fr.eni.lesobjetssontnosamis.bo.Utilisateur;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class UtilisateurController {
-
+    protected final Log logger = LogFactory.getLog(getClass());
     private Utilisateur utilisateur;
 
     public UtilisateurController(UtilisateurService utilisateurService) {
